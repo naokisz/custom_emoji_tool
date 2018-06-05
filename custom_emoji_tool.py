2bbda25219254c7f2da96d5b4f39deb20d95b182
Naoki Suzuki <naokihirokitomoki@gmail.com>
2018-06-05 11:54:14 +0900

    add --count --url arg

--- custom_emoji_tool.py
+++ custom_emoji_tool.py
@@ -1,11 +1,25 @@
 import requests
 import json
+import sys
 
-print("インスタンスのURLを入力して下さい。＞")
-instance_url = input()
-print("https://" + instance_url + "/api/v1/custom_emojis")
+args = sys.argv
+if "--url" in args:
+	instance_url = args[args.index("--url") + 1]
+else:
+	print("インスタンスのURLを入力して下さい。＞")
+	instance_url = input()
+#print("https://" + instance_url + "/api/v1/custom_emojis")
 ret = requests.get("https://" + instance_url + "/api/v1/custom_emojis")
 emoji_data = ret.json()
-#print(emoji_data)
-for i in emoji_data:
-    print(" :" + i["shortcode"] + ": ")
+
+if "--count" in args:
+	print("\n" + str(len(emoji_data)))
+
+if "--no_br" in args:
+	print("\n", end="")
+	for i in emoji_data:
+		print(":" + i["shortcode"] + ": ", end="")
+	print("\n")
+else:
+	for i in emoji_data:
+	    print(":" + i["shortcode"] + ":")
